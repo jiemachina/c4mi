3968cccc59cf26a24f225099a3fb20877c430fed
nealnote <nealnote@gmail.com>
2013-10-22 11:23:09 +0800

    update for tv; u need confirm twice if u want box or tv

--- js/c4mi.js
+++ js/c4mi.js
@@ -1,9 +1,9 @@
 /*
  * cheat for miphone
  * skip the official wait time
- * @cheat_type {'box'|'phone'},default for phone
+ * @cheat_type {'phone'|'box'|'tv'},default for phone
  */
-var cheat_type = 'phone',//cheat for box/phone,default for phone
+var cheat_type = 'phone',//cheat for box/phone/tv,default for phone
 	cheat_interval = 1000,
 	cheat_timer;
 
@@ -16,8 +16,10 @@ if ( !window['hdcontrol'] ) {
 //ask for type
 if ( window.confirm('what do u want? phone(Ok),other(Cancel)') ) {
 	cheat_type = 'phone'
-}else{
+}else if ( window.confirm('box(Ok),tv(Cancel)') ){
 	cheat_type = 'box'
+}else{
+	cheat_type = 'tv'
 }
 
 //hook
@@ -25,93 +27,112 @@ if (!window['cheat_hdcontrol']) {
 	cheat_hdcontrol = hdcontrol;
 }
 
+//get hdcontrol source
+//hdcontrol.toString().replace(/_\$\[([0-9])+\]/ig,function(src){
+//	return '"'+ eval(src) +'"';
+//});
+
 //rewrite callback
-hdcontrol = function(json){
-	//true hdcontrol need
-	isRollStatus = true;
+function hdcontrol(e) {
+	//cheat
+	isRollStatus=true;
 	isPhone=false;
 	isBox=false;
-	if ( cheat_type=='box' ) {
-		isBox = true;
-	}else{
+	isTv=true;
+	if ( cheat_type == 'phone' ) {
 		isPhone = true;
+		rollProduct = 'phone';
+	}else if( cheat_type == 'box' ) {
+		isBox = true;
+		rollProduct = 'box';
+	}else{
+		isTv = true;
+		rollProduct = 'tv';
 	}
 	//stop req when in Q
-	if (json.status.allow && (json.status.miphone.hdurl || json.status.mibox.hdurl )) {
-		clearInterval(cheat_timer);
+	if (window.cheat_timer) {
+		clearInterval(cheat_timer)
 	}
-	var phonestart = json.status.miphone.hdstart,
-		phonestop = json.status.miphone.hdstop,
-		boxstart = json.status.mibox.hdstart,
-		boxstop = json.status.mibox.hdstop;
-	servertime = downServertime = json.stime;
-	var diffTime = parseInt(servertime - miphoneBuy.localTime());
-	m.cookie("xm_difft_hd", diffTime, {
-		path: '/',
-		domain: '.xiaomi.com',
-		expires: 1
-	});
-	if (json.status.allow) {
-		if (isPhone === true) {
-			//isPhone === false;
-			if (json.status.miphone.hdurl == null || json.status.miphone.hdurl == '') {
-				//window.location.reload();
-			}
-			else {
-				location.href = 'http://t.hd.xiaomi.com/s/' + json.status.miphone.hdurl;
-			}
-		} else if (isBox === true) {
-			//isBox === false;
-			if (json.status.mibox.hdurl == '' || json.status.mibox.hdurl == null) {
-				//window.location.reload();
-			}
-			else {
-				location.href = 'http://t.hd.xiaomi.com/s/' + json.status.mibox.hdurl;
-			}
-		}
-	}
-	if (phonestart === true && phonestop === false && boxstart === false && boxstop === true) {
-		m.cookie("xm_xt_obox", 1, {
-			path: '/',
-			domain: '.xiaomi.com',
-			expires: 1
-		});
-		m.cookie('xm_xt_ophone', null, {
-			path: '/',
-			domain: '.xiaomi.com'
-		});
-		miphoneBuy.box(false);
-		stepHtml.five();
-	}
-	if (phonestart === false && phonestop === true && boxstart === true && boxstop === false) {
-		m.cookie("xm_xt_ophone", 1, {
-			path: '/',
-			domain: '.xiaomi.com',
-			expires: 1
-		});
-		m.cookie('xm_xt_obox', null, {
-			path: '/',
-			domain: '.xiaomi.com'
-		});
-		miphoneBuy.box(false);
-		stepHtml.six();
-	}
-	if (phonestart === false && phonestop === true && boxstart === false && boxstop === true) {
-		m.cookie("xm_xt_pre", 1, {
-			path: '/',
-			domain: '.xiaomi.com',
-			expires: 1
-		});
-		m.cookie('xm_xt_obox', null, {
-			path: '/',
-			domain: '.xiaomi.com'
-		});
-		m.cookie('xm_xt_ophone', null, {
-			path: '/',
-			domain: '.xiaomi.com'
-		});
-		miphoneBuy.saleOut();
-	}
+	//origin
+    var t = e.status,
+        n = t.miphone.hdurl,
+        i = t.mibox.hdurl,
+        a = t.mitv.hdurl,
+        o = t.miphone.hdstart === false && t.miphone.hdstop === true ? true : false,
+        r = t.mibox.hdstart === false && t.mibox.hdstop === true ? true : false,
+        l = t.mitv.hdstart === false && t.mitv.hdstop === true ? true : false,
+        s = e.stime + (0x8 - (new Date).getTimezoneOffset() / 0x3c * -0x1) * 0xe10;
+    servertime = downServertime = s;
+    var c = parseInt(servertime - miphoneBuy.localTime());
+    m.cookie("xm_difft_hd", c, {
+        path: "/",
+        domain: ".xiaomi.com",
+        expires: 0x1
+    });
+
+    function d(e) {
+        if (e == null || e == "") {
+            //window.location.reload()
+        } else {
+            location.href = "http://t.hd.xiaomi.com/s/" + e
+        }
+    };
+
+    function h(e) {
+        m.cookie("xm_xt_prex", e, {
+            path: "/",
+            domain: ".xiaomi.com",
+            expires: 0x1
+        })
+    };
+    if (isRollStatus) {
+        if (t.allow) {
+            if (isPhone === true) {
+                isPhone = false;
+                d(n)
+            } else if (isBox === true) {
+                isBox = false;
+                d(i)
+            } else if (isTv === true) {
+                isTv = false;
+                d(a)
+            }
+        };
+        isRollStatus = false
+    };
+    if (l || o || r) {
+        if (window.timeInter) {
+            clearInterval(timeInter)
+        }
+    };
+    if (l && rollProduct === "tv" || o && rollProduct === "phone" || r && rollProduct === "box") {
+        miphoneBuy.box(false)
+    };
+    if (l && o && r) {
+        h(0x4);
+        miphoneBuy.saleOut();
+        if (window.endInter) {
+            clearInterval(endInter)
+        }
+    } else if (l && o && !r) {
+        h(0x7);
+        stepHtml.seven()
+    } else if (l && !o && !r) {
+        h(0x5);
+        stepHtml.five()
+    } else if (l && !o && r) {
+        h(0x8);
+        stepHtml.eight()
+    } else if (!l && o && r) {
+        h(0x9);
+        stepHtml.nine()
+    } else if (!l && !o && r) {
+        h(0xa);
+        stepHtml.ten()
+    } else if (!l && o && !r) {
+        h(0x6);
+        stepHtml.six()
+    }
 }
 //send req
 cheat_timer = setInterval(function(){
